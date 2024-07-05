5b8fe58269da7b61e4559249a708202d1abc18ce
Dave McCollough <davemccollough@gmail.com>
2024-07-04 20:36:06 -0600

    Initial Commit

---
+++ SwiftUIIntro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIIntro
+//
+//  Created by Dave McCollough on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIIntro/SwiftUIIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIntroApp.swift
+//  SwiftUIIntro
+//
+//  Created by Dave McCollough on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
